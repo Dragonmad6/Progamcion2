d10c1d1aa6b58742c7bf21f2d110532d47404336
CMaster <CMaster@DESKTOP-K2CQ5G0>
2020-10-31 12:50:14 +0100

    hajbvfdiuasboif

---
+++ Programación2/src/UT4/holamundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package UT4;
+
+/**
+ * @author CMaster
+ *
+ */
+public class holamundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
